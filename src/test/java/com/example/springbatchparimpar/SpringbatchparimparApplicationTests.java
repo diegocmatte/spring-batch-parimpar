d737b742b802b7e47008d7c7f3e6fe60af57ea4d
Diego Matté <diegocmatte@gmail.com>
2021-10-11 19:37:54 -0300

    criado projeto

---
+++ src/test/java/com/example/springbatchparimpar/SpringbatchparimparApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchparimpar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchparimparApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
